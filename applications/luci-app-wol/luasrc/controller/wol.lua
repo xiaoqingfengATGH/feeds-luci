47f2d595367dd75a62a1d2acc1fb510790333f1e
xiaoqingfeng <xiaoqingfengatgm@gmail.com>
2020-05-10 09:04:05 +0800

    Adjust luci-app-wol menu entry

--- applications/luci-app-wol/luasrc/controller/wol.lua
+++ applications/luci-app-wol/luasrc/controller/wol.lua
@@ -1,6 +1,6 @@
 module("luci.controller.wol", package.seeall)
 
 function index()
-	entry({"admin", "services", "wol"}, form("wol"), _("Wake on LAN"), 90)
-	entry({"mini", "services", "wol"}, form("wol"), _("Wake on LAN"), 90)
+	entry({"admin", "control", "wol"}, form("wol"), _("Wake on LAN"), 90)
+	entry({"mini", "control", "wol"}, form("wol"), _("Wake on LAN"), 90)
 end
